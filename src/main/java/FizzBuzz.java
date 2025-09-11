6bdea9f809739e2996f5e2679fbf055e8d10774d
SHULER WIEGERINK <55ab74b0-b2ac-4a95-9a24-3bc2700dfca8@KAM-CA-H4TG50PGQ6XG.local>
2025-09-11 13:57:21 -0400

    fizzbuzz program

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -9,6 +9,27 @@ public class FizzBuzz {
      * Output should be 100 whitespace-separated tokens (spaces or newlines are fine).
      */
     public static void main(String[] args) {
-        // TODO
+        for(int i = 1; i < 100; i++)
+        {
+            if((i % 3) == 0)
+            {
+                if((i % 5) == 0)
+                {
+                    System.out.println("FizzBuzz");
+                }
+                else
+                {
+                    System.out.println("Fizz");
+                }
+            }
+            else if((i % 5) == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
     }
 }
